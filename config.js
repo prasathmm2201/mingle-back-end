536131876776e3adfff90d324675d3f2f95509a9
prasathmm2201 <140078938+prasathmm2201@users.noreply.github.com>
2024-07-11 14:31:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
+import dotenv from "dotenv";
+
+dotenv.config(".env");
+
 const {PORT , DB_USERNAME, DB_HOST,DB_PORT,DB_NAME,DB_PASSWORD , HASH , JWT , CLIENT_ID , CLIENT_HOST,CLIENT_SECRET,FRONT_END_URL} = process.env
 export const config = {
     PORT,
